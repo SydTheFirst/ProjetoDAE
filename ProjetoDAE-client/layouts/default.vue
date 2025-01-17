fdc7112a4775c2954cc6aff03bf367e7dade1aaf
2202318RicardoTome <2202318@my.ipleiria.pt>
2025-01-17 16:57:22 +0000

    Css 10%

---
+++ ProjetoDAE-client/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- Cabeçalho de Navegação -->
+    <Nav />
+
+    <!-- Conteúdo Principal -->
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Nav from "@/components/Nav.vue"; // Importando o componente Nav
+</script>
+
+<style>
+/* Estilos adicionais do layout */
+</style>
---
+++ ProjetoDAE-client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app.vue',
+    './pages/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './server/**/*.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
